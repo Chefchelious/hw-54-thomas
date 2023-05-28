30256747a4a3ee3b09886f99a6e5b0d9398eb15e
Dmitrii Morrows <trollman1993@gmail.com>
2023-05-28 21:34:32 +0600

    создана функция createItems, создающая массив из 36 объектов и одним рандомным объектом со свойстсвом hasItem: true. С помощью этой функции был инициализирован начальный стейт.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     sgfsf
-    </div>
-  );
-}
+type TItem = {
+    hasItem: boolean;
+    clicked: boolean
+};
+
+const App = () => {
+    const createItems = (): TItem[] => {
+        const items: TItem[] = [];
+
+        for (let i: number = 0; i < 36; i++) {
+            const item: TItem = {
+                hasItem: false,
+                clicked: false
+            };
+            items.push(item);
+        }
+
+        const randomIndex: number = Math.floor(Math.random() * items.length);
+        items[randomIndex] = {
+            hasItem: true,
+            clicked: false
+        };
+
+        return items;
+    };
+
+    const [items, setItems] = useState(createItems);
+    console.log(items);
+
+    return (
+        <div className="App">
+
+        </div>
+    );
+};
 
 export default App;
